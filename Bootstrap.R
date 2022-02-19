ceee15daa3e170d251d9db7b1db49a1e2e0b03fd
sonnleit <maximilian.sonnleitner@edu.fh-joanneum.at>
2022-02-19 19:37:01 +0100

    change

--- Bootstrap.R
+++ Bootstrap.R
@@ -1,7 +1,7 @@
 #--------------Bootstraping---------------------
 listofdfs <- list()
 
-
+## Sample of 100 tweets per topic
 for (i in c(7:13)){
   alg.data %>% 
     filter(alg.data[,i]>=1)%>% 
@@ -38,49 +38,11 @@ results <- boot(data=as.vector(ObservedHeights), statistic=ReturnMean, R=10000)
 
 
 hist(results$t)
-
-boot.ci(results, type="bca")
-
 results
 
-
-
 # Bootstrap 95% CI for R-Squared
-
-# function to obtain R-Squared from the data 
-rsq <- function(formula, data, indices) {
-  d <- data[indices,] # allows boot to select sample 
-  fit <- lm(formula, data=d)
-  return(summary(fit)$r.square)
-} 
-
-results <- boot(data=bootstrap, statistic=rsq, 
-                R=100, formula=Positive~Negative)
-
-# view results
-results 
-plot(results)
-
-# get 95% confidence interval 
 boot.ci(results, type="bca")
 
 
 
-# Author DataFlair
 
-# Creating Function to obtain R-Squared from the data
-r_squared <- function(formula, data, indices) {
-  val <- data[indices,4] # selecting sample with boot 
-  fit <- lm(formula, data=val)
-  return(summary(fit)$r.square)
-} 
-# Performing 1500 replications with boot 
-output <- boot(data=bootstrap, statistic=r_squared, 
-               R=1500)
-
-# Plotting the output
-output 
-plot(output)
-
-# Obtaining a confidence interval of 95%
-boot.ci(output, type="bca")
